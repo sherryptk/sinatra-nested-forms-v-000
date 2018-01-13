4c8b10a49c7713dd473a88e8938e1911aae3dd56
Sherry El Garhy <sherryptk@gmail.com>
2018-01-13 18:43:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ module FormsLab
       erb :'pirates/new'
     end
 
-    post '/show' do
+    post '/pirates' do
       @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
 
       params[:pirate][:ships].each do |details|
@@ -21,7 +21,7 @@ module FormsLab
 
       @ships = Ship.all
 
-      erb :show
+      erb :'pirates/show'
     end
 
   end
