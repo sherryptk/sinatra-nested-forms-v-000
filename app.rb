f618d5e5c6b8eadb185a8dff1ab91c8f85f1ab85
Sherry El Garhy <sherryptk@gmail.com>
2018-01-13 18:43:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ module FormsLab
       @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
 
       params[:pirate][:ships].each do |details|
+        binding.pry
         Ship.new(details)
       end
 
